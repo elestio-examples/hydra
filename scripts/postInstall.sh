30a9522851f1204a7e58fac4bd90a2a16fca40d1
David <30422759+davalimi@users.noreply.github.com>
2024-03-21 17:32:22 +0200

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,7 +1,7 @@
 set -o allexport; source .env; set +o allexport;
 
 docker-compose down
-openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
+openssl req -new -x509 -days 3650 -nodes -text -out ./hydra/db/server.crt -keyout ./hydra/db/server.key -subj "/CN=${CNAME}"
 sleep 5s;
 sed -i 's/#command:/command:/' docker-compose.yml
 docker-compose up -d;
